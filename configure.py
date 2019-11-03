5efbe2a2d534b7bc63fcb42e67179b72ed4c5cc0
Austin Riffle <austinr1028@gmail.com>
2019-11-03 14:20:29 -0600

    Working build for src folder

--- configure.py
+++ configure.py
@@ -7,14 +7,15 @@ def create_venv(venv_dirname):
     result.check_returncode()
 
 def install_requirements(venv_interpreter):
-    result = subprocess.run([sys.executable, '-m', 'pip', 'install', '-r', 'py_requirements.txt'])
+    result = subprocess.run([venv_interpreter, '-m', 'pip', 'install', '-r', 'py_requirements.txt'])
     result.check_returncode()
 
 if __name__ == '__main__':
     venv_dirname = os.getcwd() + '/' + '.venv'
+    print(f"Creating python virtual environment at '{venv_dirname}'")
     create_venv(venv_dirname)
     print(f"Created python virtual environment at '{venv_dirname}'")
 
+    print(f"Installing project dependencies")
     install_requirements(venv_dirname + "/bin/python")
     print(f"Installed all project dependencies")
-

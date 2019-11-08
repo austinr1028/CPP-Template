c325a675467bc9a502d81c5995c8567df94e01c3
Austin Riffle <austinr1028@gmail.com>
2019-11-07 22:48:15 -0600

    #6: Basic Travis CI support

--- configure.py
+++ configure.py
@@ -6,8 +6,8 @@ def create_venv(venv_dirname):
     result = subprocess.run([sys.executable, '-m', 'venv', venv_dirname])
     result.check_returncode()
 
-def install_requirements(venv_interpreter):
-    result = subprocess.run([venv_interpreter, '-m', 'pip', 'install', '-r', 'py_requirements.txt'])
+def install_requirements(venv_pip):
+    result = subprocess.run([venv_pip, 'install', '-r', 'requirements.txt'])
     result.check_returncode()
 
 if __name__ == '__main__':
@@ -17,5 +17,5 @@ if __name__ == '__main__':
     print(f"Created python virtual environment at '{venv_dirname}'")
 
     print(f"Installing project dependencies")
-    install_requirements(venv_dirname + "/bin/python")
+    install_requirements(venv_dirname + "/bin/pip")
     print(f"Installed all project dependencies")
